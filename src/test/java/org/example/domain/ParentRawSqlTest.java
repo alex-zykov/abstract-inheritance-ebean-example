0d08a5f8a470425f635bd3972b8ec815673885db
Alexey Zykov <ale.zykov@gmail.com>
2015-07-12 17:40:35 +0200

    4.5.3 final example

--- src/test/java/org/example/domain/ParentRawSqlTest.java
+++ src/test/java/org/example/domain/ParentRawSqlTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class ParentRawSqlTest extends ExampleBaseTestCase {
 
     @Test
-    public void RawSqlParentCollectionFetch() {
+    public void RawSqlParentLoad() {
         List<Data> exampleData = new ArrayList<>();
         exampleData.add(new Data(0));
         exampleData.add(new Data(1));
@@ -33,7 +33,36 @@ public class ParentRawSqlTest extends ExampleBaseTestCase {
         Ebean.save(c);
 
         RawSql rawSql = RawSqlBuilder
-                .unparsed("select id from parent where number > 1")
+                .parse("select type, id, number from parent where number > 1")
+                .create();
+        List<Parent> partial = Ebean.find(Parent.class).setRawSql(rawSql)
+                .findList();
+        assertNotNull(partial.get(0).getData());
+        assertEquals(partial.get(0).getData().get(0).getNumber().intValue(),0);
+    }
+
+    @Test
+    public void RawSqlParentFetch() {
+        List<Data> exampleData = new ArrayList<>();
+        exampleData.add(new Data(0));
+        exampleData.add(new Data(1));
+        exampleData.add(new Data(2));
+
+        ChildA a = new ChildA(0);
+        a.setData(exampleData);
+        Ebean.save(a);
+
+        ChildB b = new ChildB(1);
+        b.setData(exampleData);
+        Ebean.save(b);
+
+        ChildA c = new ChildA(2);
+        c.setData(exampleData);
+        Ebean.save(c);
+
+        RawSql rawSql = RawSqlBuilder
+                .unparsed("select type, id from parent where number > 1")
+                .columnMappingIgnore("type")
                 .columnMapping("id", "parent.id")
                 .create();
         List<ParentAggregate> aggregates = Ebean.find(ParentAggregate.class).setRawSql(rawSql)
